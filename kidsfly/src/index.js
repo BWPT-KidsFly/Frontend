6c716c7b463b660bb011012d79fb98e5c574c73d
tucker <tuckerwray84@gmail.com>
2019-12-20 01:15:55 -0500

    finish setting up the Provider and Redux store

--- kidsfly/src/index.js
+++ kidsfly/src/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom';
+import { createStore, compose, applyMiddleware } from 'redux'
+import { rootReducer } from './store/reducers'
 import App from './components/App';
-import {BrowserRouter as Router} from 'react-router-dom';
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+import './index.css';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+const stores = createStore(rootReducer,/*preloadedState,*/ composeEnhancers(applyMiddleware(thunk, logger)
+))
+
+ReactDOM.render(
+    <Provider store={stores}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>,
+    document.getElementById('root')
+);
 
