b577281d743c4f816fbfcc677177681a945cfc37
Adrian Guadalupe <safisweetkeyz@gmail.com>
2019-12-21 15:57:18 -0500

    moved index.css into components dirctory to try to fix an error

--- kidsfly/src/index.js
+++ kidsfly/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './components/index.css';
 import App from './components/App';
 import {BrowserRouter as Router} from 'react-router-dom';
+import './components/index.css';
 
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
