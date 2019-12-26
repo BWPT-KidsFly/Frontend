eed319c9fe998db6aa153473d3a62b397283c8cb
Adrian Guadalupe <safisweetkeyz@gmail.com>
2019-12-26 13:30:56 -0500

    nav and forms integrated

--- kidsfly/src/components/App.js
+++ kidsfly/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import Navigation from './Navigation';
 import TravelerSignUp from './TravelerSignUp';
 import StaffSignUp from './StaffSignUp';
 import SignUpAs from './SignUpAs';
@@ -9,6 +10,7 @@ import LogInAs from './LogInAs';
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <Route exact path='/' />
       <Route exact path='/sign-up' component={SignUpAs}/>
       <Route path='/sign-up/traveler' component={TravelerSignUp} />
