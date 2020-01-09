d6d605cbd3c75b52e1b69656892fe005a503794c
Adrian Guadalupe <safisweetkeyz@gmail.com>
2020-01-09 11:48:29 -0500

    added reactstrap and bootstrap

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './components/index.css';
 import App from './components/App';
 import {BrowserRouter as Router} from 'react-router-dom';
 import './components/index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
