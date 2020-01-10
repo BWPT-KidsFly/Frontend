2e4e5727e3ed18268ef8fd45ae142da8e0e624e8
Adrian Guadalupe <safisweetkeyz@gmail.com>
2020-01-09 21:33:45 -0500

    merge fix

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -3,13 +3,9 @@ import React from 'react';
 const Stats = ({ traveler }) => {
    return (
       <div className='stats'>
-            <p>Born: {traveler.birth_year}</p>
-            <p>Gender: {traveler.gender}</p>
-            <p>Skin: {traveler.skin_color}</p>
-            <p>Hair: {traveler.hair_color}</p>
-            <p>Eyes: {traveler.eye_color}</p>
-            <p>Height: {traveler.height}</p>
-            <p>Mass: {traveler.mass}</p>
+            <p>id: {traveler.id}</p>
+            <p>username: {traveler.username}</p>
+            <p>password: {traveler.password}</p>
          </div>
    );
 };          
