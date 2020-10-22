d46d0b1b2f7679d4634e48acf76924888dc9e221
tucker <tuckerwray84@gmail.com>
2020-10-22 17:56:25 -0400

    ## AddUser - import DynamoDBs DocumentClient fron AWS SDK - construct a main handler to determine wiichvoked - writes method to add a user to the usd with appropriate status codde and messages tables and response :

--- serverless/index.js
+++ serverless/index.js
@@ -1,23 +1,49 @@
-exports.handler = async (event) => {
-  console.log({ event });
-  let response = {};
-  if (event.key1 === "error") {
-    response = {
-      statusCode: 500,
-      body: JSON.stringify("Oh No something is wrong."),
-    };
-  } else if (event.key1 === "success") {
-    response = {
-      statusCode: 200,
-      body: JSON.stringify("Everything is kosher and groovy."),
-    };
-  } else {
-    response = {
-      statusCode: 400,
-      body: JSON.stringify(
-        "Not sure what you want. Unauthorized Access Attempt logged."
-      ),
-    };
-  }
-  return response;
+const {DynamoDB}= require("aws-sdk"),
+api={},
+TABLE_NAME=process.env.TABLE_NAME;
+
+
+api.handler= async(event)=>{
+    console.log({event});
+    let response={};
+    try{
+        if (event.path.includes('users') && event.httpMethod==="POST"){
+            
+// SAVE USER IN DynaomoDB
+// parse the JSON string event body into and pass to create handler set response with table's output and success code
+// if rejected refer to catch statement
+        response.body=JSON.stringify(await api.handleCreateUser(JSON.parse(event.body)));
+        response.statusCode=201; 
+        }
+    }catch(e){
+// set response to JSON version of error and status code for serverside error non auth0/authN
+        response.body=JSON.stringify(e);
+        response.statusCode=500;
+    }
+// log final form of respopnse to cloudwatch and return to client
+        console.log(
+            "1",{response},
+            "2","response",
+            "3",response
+            );
+    return response;
+}
+   
+api.handleCreateUser= async(userObject)=>{
+ return new Promise((resolve,reject)=>{
+     let documentClient= new DynamoDB.DocumentClient();
+     
+     let params={ 
+         TableName:TABLE_NAME,
+         Item:item
+     };
+     
+     documentClient.put(params,(err,data)=>{
+         if(err) reject(err);
+         else resolve(data)
+    });
+ 
+ })
 };
+
+module.exports=api;
